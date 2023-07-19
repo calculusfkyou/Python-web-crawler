96a4ca475b40a2ba7a18599bd194286accf5639b
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-19 22:29:47 +0800

    app.py

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def handle_message(event):
 # main
 # 主程式
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
